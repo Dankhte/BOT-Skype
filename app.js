b15a1ac0e25072e75848859d672db9ce86e7f2b6
Ismael Arteseros <iarteseros@dome-consulting.com>
2017-05-05 13:20:21 +0200

    Primer commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//Bot on
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+                .address(message.address)
+                .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'everyone');
+        bot.send(reply);
+    } else {
+        // delete their data
+    }
+});
+bot.on('typing', function (message) {
+  // User is typing
+});
+bot.on('deleteUserData', function (message) {
+    // User asked to delete their data
+});
+//=========================================================
+// Bots Dialogs
+//=========================================================
+String.prototype.contains = function(content){
+  return this.indexOf(content) !== -1;
+}
+var reiniciando = false;
+bot.dialog('/', function (session) {
+	var name = session.message.user ? session.message.user.name : null;
+	var message = session.message.text.toLowerCase();
+    if(message.contains('reinicio') || message.contains('reiniciamos') || message.contains('reiniciar') || message.contains('reiniciaremos')){
+      session.send('%s, avisanos cuando esté levantado por favor.', name || 'Tú, el del reinicio');
+      reiniciando = true;
+	  }
+	  if(reiniciando && (message.contains('ya esta') || message.contains('ya está') || message.contains('listo') || message.contains('reiniciado'))){
+      session.send('Seguro %s?, muchas gracias.', name || 'seguro');
+      reiniciando = false;
+	  }
+	});
